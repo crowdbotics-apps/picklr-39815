f33a801301aa03b8bba5d20108cd816ec8a22ee2
Crowdbotics <team@crowdbotics.com>
2023-04-10 17:54:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "picklr-39815",
+  url: "https://picklr-39815.botics.co",
+  api: "https://picklr-39815.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
